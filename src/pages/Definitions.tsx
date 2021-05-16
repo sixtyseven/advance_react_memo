216d798dccf04570259150f178a939aec1923585
Bruce <lsq991@gmail.com>
2021-05-16 14:30:31 +1000

    add immutable data definition

--- src/pages/Definitions.tsx
+++ src/pages/Definitions.tsx
@@ -3,31 +3,35 @@
 */
 
 interface IItem {
-  label: string;
-  link: string;
+  label: string
+  link: string
 }
 
 const definitionList: IItem[] = [
   {
-    label: "React.Component",
-    link: "https://reactjs.org/docs/react-api.html#reactComponent",
+    label: 'React.Component',
+    link: 'https://reactjs.org/docs/react-api.html#reactComponent',
   },
   {
-    label: "React.PureComponent",
-    link: "https://reactjs.org/docs/react-api.html#reactpureComponent",
+    label: 'React.PureComponent',
+    link: 'https://reactjs.org/docs/react-api.html#reactpureComponent',
   },
   {
-    label: "React.memo",
-    link: "https://reactjs.org/docs/react-api.html#reactmemo",
+    label: 'React.memo',
+    link: 'https://reactjs.org/docs/react-api.html#reactmemo',
   },
   {
-    label: "useCallback",
-    link: "https://reactjs.org/docs/hooks-reference.html#usecallback",
+    label: 'useCallback',
+    link: 'https://reactjs.org/docs/hooks-reference.html#usecallback',
   },
-];
+  {
+    label: 'Immutable Data',
+    link: 'https://redux.js.org/faq/immutable-data',
+  },
+]
 
 function Definitions() {
-  console.log("[render] Definitions page");
+  console.log('[render] Definitions page')
   return (
     <div className="page">
       <h3>Definitons</h3>
@@ -39,12 +43,12 @@ function Definitions() {
                 {item.label}
               </a>
             </li>
-          );
+          )
         })}
       </ul>
       <p>Definitions are mainly from https://reactjs.org/docs/react-api.html</p>
     </div>
-  );
+  )
 }
 
-export default Definitions;
+export default Definitions
